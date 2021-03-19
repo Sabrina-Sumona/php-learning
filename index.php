8ab5a4c94b4e6192b89821ce84a2ee0769f4f027
Sabrina Sumona <snsbauet04@gmail.com>
2021-03-19 19:00:11 +0600

    function

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 // echo is used for showing the output
 echo 'Hello World!!';
+
 // br tag of html
 echo '<br>';
+
 // variables are declared by using $ symbol
 // we don't need to declare the data type of the variable
 // variable can start with _ , lower case, upper case
@@ -10,9 +12,11 @@ echo '<br>';
 // and can't start with a number
 $num = 30;
 echo "number1 = ";
+
 // variable needs $ sign to echo
 echo $num;
 echo '<br>';
+
 // constant 
 // it contains uppercases, which is a good practice
 // but we can also use lowercases
@@ -22,6 +26,7 @@ echo "number2 = ";
 // constant does not need $ sign to echo
 echo NUM;
 echo '<br>';
+
 // for string we can use "" or '' but not ``
 $name = "Sabrina";
 // we can concate a variable into another string
@@ -32,12 +37,14 @@ $name = "Sumona";
 // we can also concate with . operator
 echo $th.$name;
 echo '<br>';
+
 // data types in php : number & string
 $half= 0.5;
 $result= $half*2+2;
 $val= "The result is : ".$result/2;
 echo $val;
 echo '<br>';
+
 // array using []
 $arr = ['Sabrina', 'Naorin', 'Sumona'];
 // cant echo the array at once as echo print a string at a time
@@ -50,14 +57,17 @@ echo $arr[0];
 echo '<br>';
 echo $arr[2];
 echo '<br>';
+
 // another way to declare array
 $arr1 = array('Minu', 'Tinu', 'Tushi');
 print_r($arr1[2]);
 echo '<br>';
+
 // key of index declaration of array
 $arr2 = array('1st'=>'Minu', '2nd'=>'Tinu', '3rd'=>'Tushi');
 print_r($arr2['1st']);
 echo '<br>';
+
 $arr3= ['1st'=>'Sabrina', '2nd'=>'Naorin', '3rd'=>'Sumona'];
 print_r($arr3);
 echo '<br>';
@@ -67,18 +77,21 @@ echo $arr3['2nd'];
 echo '<br>';
 echo $arr3['3rd'];
 echo '<br>';
+
 // we can update an array value
 $arr3['3rd'] = 'Sumu';
 print_r($arr3['3rd']);
 echo '<br>';
 print_r($arr3);
 echo '<br>';
+
 // for loop 
 for($i=0; $i<=10; $i++){
     echo "1 * $i = ".$i."<br>";
 }
 echo "For Loop closed.";
 echo '<br>';
+
 // while loop
 $i=0;
 $number= ['8', '9', '2', '4', '7'];
@@ -88,6 +101,7 @@ while($i<=2*count($number)){
     $i++;
 }
 echo "While Loop closed.";
+
 echo '<br>';
 // for each loop
 // this loop traverse a array for each index
@@ -97,10 +111,38 @@ foreach($fruits as $fruit){
     echo $fruit.'<br>';
 }
 echo "For each Loop closed.<br>";
+
 $relatives= ['Father'=>'Muslem', 'Mother'=>'Rumisa', 'Sister'=>'Rumana', 'Brother'=>'Maruf'];
 foreach($relatives as $key=> $val){
     echo $key.'\'s name is '.$val.'<br>';
 }
 echo "For each Loop closed.";
 echo '<br>';
+
+//functions
+function showName(){
+    echo "My name is Sabrina Sumona".'<br>';
+}
+showName();
+
+function showName2($name){
+    echo "My name is $name".'<br>';
+}
+showName2('Sabrina');
+showName2('Sumona');
+
+function showName3($name='Naorin', $n=0){
+    echo "My name is ". $name.'<br>';
+    return $n+1;
+}
+$n= 1;
+$num = showName3('Minu', $n);
+echo 'num = '.$num.'<br>';
+$n= 5;
+$num= showName3('Tushi', $n);
+echo 'num = '.$num.'<br>';
+
+// if we don't pass any value, the default value will be shown
+$num = showName3();
+echo 'num = '.$num.'<br>';
 ?>
