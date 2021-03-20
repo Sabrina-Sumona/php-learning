de6ef94cc082fd5a6159c5194c72ed6afaee0dda
Sabrina Sumona <snsbauet04@gmail.com>
2021-03-20 10:44:06 +0600

    inheritance , encapsulation, polymorphism,  abstraction

--- oopPhp.php
+++ oopPhp.php
@@ -7,7 +7,6 @@
 // class name starts with uppercase letter
 class Vehicle{
     public $wheels;
-
     public function vehicleName($name){
         echo "The name of this vehicle is $name".'<br>';
     }
@@ -28,7 +27,9 @@ $bus->vehicleName('bus');
 // constructors & destructors
 
 class Vehicle2{
-    public $wheels;
+    // public $wheels;
+    protected $wheels;
+    // private $wheels;
     public function __construct($wheel)
     {
         $this->wheels= $wheel;
@@ -53,102 +54,149 @@ print_r($bus);
 echo '<br>';
 $bus->vehicleName2('bus');
 
-// class Vehicle{
-//     public $wheels;
+// inheritance 
+class Motorcycle1 extends Vehicle{
+    public $capacity;
+    public function Motorcapacity ($capacity){
+        echo "The capacity of this motorcycle is $capacity".'<br>';
+    }
 
-//     public function vehicleName($name){
-//         echo "The name of this vehicle is $name";
-//     }
-// }
-// class Motorcycle extends Vehicle{
-//     public $weight;
+}
 
-// }
+$honda= new Motorcycle1();
+$honda->wheels=2;
+$honda->capacity='100cc';
+print_r($honda);
+echo '<br>';
+$honda->vehicleName('hornet');
 
-// $honda= new Motorcycle();
-// //echo $honda->vehicleName('hornet');
-// $honda->wheels=4;
-// echo $honda->wheels;
+// Encapsulation
+// Access Modifiers : public, protected, private
+// public access modifiers is used to access the class variable/function from everywhere
+// protected access modifiers is used to access the class variable/function within that class or it's derived class
+// private access modifiers is used to access the class variable/function within that class only
 
-// class Vehicle{
-//     protected $wheels;
-//     public function __construct($wheel){
-//         $this->wheels= $wheel;
-//     }
-//     public function vehicleName($name){
-//         echo "The name of this vehicle is $name";
-//     }
-// }
-// class Motorcycle extends Vehicle{
-//     public $weight;
-//     public function display(){
-//         echo $this->wheels;
-//     }
-// }
+class Motorcycle2 extends Vehicle2{
+    public $capacity;
+    public function Motorcapacity2 ($capacity){
+        echo "The capacity of this motorcycle is $capacity".'<br>';
+    }
+    public function display(){
+        echo "The wheels of this motorcycle are $this->wheels".'<br>';
+    }
+}
 
-// $honda= new Motorcycle('2');
-// //echo $honda->vehicleName('hornet');
-// //$honda->wheels=4;
+$honda= new Motorcycle2(2);
+$honda->capacity='100cc';
+print_r($honda);
+echo '<br>';
+$honda->vehicleName2('hornet');
+$honda->display();
+
+// Polymorphism
+class Motorcycle extends Vehicle2{
+    public function display(){
+        echo "The wheels of this motorcycle are $this->wheels".'<br>';
+    }
+}
+class Car extends Vehicle2{
+    public function display(){
+        echo "The wheels of this car are $this->wheels".'<br>';
+    }
+}
+class Bus extends Vehicle2{
+    public function display(){
+        echo "The wheels of this bus are $this->wheels".'<br>';
+    }
+}
+$honda= new Motorcycle(2);
+$honda->display();
+$car= new Car(4);
+$car->display();
+$bus= new Bus(6);
+$bus->display();
+
+// interface
+interface Vehicle3{
+    public function display();
+}
+class Motorcycle3 implements Vehicle3{
+    public $wheels;
+    public function __construct($wheel)
+    {
+        $this->wheels= $wheel;
+    }
+    public function display(){
+        echo "The wheels of this motorcycle are $this->wheels".'<br>';
+    }
+}
+class Car3 implements Vehicle3{
+    public $wheels;
+    public function __construct($wheel)
+    {
+        $this->wheels= $wheel;
+    }
+    public function display(){
+        echo "The wheels of this car are $this->wheels".'<br>';
+    }
+}
+class Bus3 implements Vehicle3{
+    public $wheels;
+    public function __construct($wheel)
+    {
+        $this->wheels= $wheel;
+    }
+    public function display(){
+        echo "The wheels of this bus are $this->wheels".'<br>';
+    }
+}
+function showValue(Vehicle3 $v){
+        $v->display();
+    }
+$honda= new Motorcycle3(2);
+showValue($honda);
 // $honda->display();
+$car= new Car3(4);
+showValue($car);
+// $car->display();
+$bus= new Bus3(6);
+showValue($bus);
+// $bus->display();
 
-// interface Vehicle{
-//     public function vehicleWheel();
-// }
-// class Motorcycle implements Vehicle{
-//     public $wheels;
-//     public function __construct($wheel){
-//         $this->wheels=$wheel;
-//     }
-//     public function vehicleWheel(){
-//         echo "Number of wheels of motorcycle is $this->wheels";
-//     }
-// }
+// Abstraction
+// we can't create any objects of abstract class
+// we only can extend/ inherit the abstract class
+abstract class Vehicle4{
+    public $wheels;
+    public function vehicleName($name){
+        echo "The name of this vehicle is $name";
+        echo '<br>';
+    }
+}
+class Car4 extends Vehicle4{
+}
 
-// class Car  implements Vehicle{
-//     public $wheels;
-//     public function __construct($wheel){
-//         $this->wheels=$wheel;
-//     }
-//     public function vehicleWheel(){
-//         echo "Number of wheels of car is $this->wheels";
-//     }
-// }
+$v= new Car4;
+$v->vehicleName('car');
 
-// class Bus  implements Vehicle{
-//     public $wheels;
-//     public function __construct($wheel){
-//         $this->wheels=$wheel;
-//     }
-//     public function vehicleWheel(){
-//         echo "Number of wheels of bus is $this->wheels";
-//     }
-// }
+abstract class Vehicle5{
+    public $wheels;
+    public function vehicleName($name){
+        echo "The name of this vehicle is $name";
+        echo '<br>';
+    }
+    abstract function showName();
+}
+// the child class must contain the definition of the abstract class's abstract function
+class Car5 extends Vehicle5{
+    public function showName()
+    {
+        echo 'Function Called';
+        echo '<br>';
+    }
+}
 
-// function showValue(Vehicle $v){
-//     $v->vehicleWheel();
-// }
-
-// $showWheels= new Car('4');
-// showValue($showWheels);
-
-// abstract class Vehicle{
-//     public $wheels;
-
-//     public function vehicleName($name){
-//         echo "The name of this vehicle is $name";
-//     }
-
-//     abstract function showName();
-// }
-
-// class Car extends Vehicle{
-//     public function showName()
-//     {
-//         echo 'Function Called';
-//     }
-// }
-
-// $v= new Car;
-// $v->vehicleName('car');
-// $v-> showName();
+$v= new Car5;
+$v->vehicleName('car');
+$v-> showName();
 ?>
